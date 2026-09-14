022e7569596594ca82da666db01a03cc53149864
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-09-14 09:24:39 +0800

    push

--- create_user.php
+++ create_user.php
@@ -29,34 +29,6 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . mysqli_error($conn);
 
 }
-<?php
-/*
-Course Code & Name : DFP50193 - Web Programming
-Full Name          : Wardah Haslan
-Registration Number: [Your Registration Number]
-Class              : DDT7B
-*/
 
-include 'db.php';
 
-$username = "admin";
-$password = "admin123";
-$full_name = "Administrator";
-
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-$sql = "INSERT INTO users (username, password, full_name)
-        VALUES ('$username', '$hashed_password', '$full_name')";
-
-if (mysqli_query($conn, $sql)) {
-
-    echo "User created successfully.<br>";
-    echo "Username: admin<br>";
-    echo "Password: admin123";
-
-} else {
-
-    echo "Error: " . mysqli_error($conn);
-
-}
 ?>
