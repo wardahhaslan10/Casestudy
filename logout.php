5d8c707be1149ad617949a34cced3ac376154fd8
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-09-14 08:16:03 +0800

    push

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Course Code & Name : DFP50193 - Web Programming
-Full Name          : ______________________________
-Registration Number: ______________________________
-Class              : ______________________________
+Full Name          : __wardha binti haslan____________________________
+Registration Number: _18ddt23f1099_____________________________
+Class              : _______ddt7b_______________________
 */
 
 session_start();
